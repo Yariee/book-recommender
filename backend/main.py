9a6a3388c86f7e27320fa823220c9dbd29ab4336
Yair <ballinasyair@gmail.com>
2026-02-27 20:51:38 -0600

    connected Anthropic API to recommend endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
+import anthropic
 
 load_dotenv()
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 app = FastAPI()
+
 
 # Defining the shape of the data we are expecting to recieve
 class BookTitle(BaseModel):
@@ -19,4 +21,15 @@ async def root():
 # When POST request hits, FastAPI will parse the JSON body and handles a BookTitle as title
 @app.post("/recommend")
 def create_title(title: BookTitle):
-    return {"book_name": title.book_name}
+    client = anthropic.Anthropic()
+    message = client.messages.create(
+        model="claude-haiku-4-5-20251001",
+        max_tokens=1000,
+        messages=[
+            {
+                "role": "user",
+                "content": f"I just read {title.book_name}, what are 5 books you recommend?"
+            }
+        ]
+    )
+    return {"recommendations": message.content[0].text}
